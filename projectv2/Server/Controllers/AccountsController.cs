847438ba7bd214852e7fb9dffdf964c67c56f3a0
Abdullah Tariq <adiabdullah1436@gmail.com>
2023-05-17 02:41:18 +0500

    Add project files.

---
+++ projectv2/Server/Controllers/AccountsController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using projectv2.Server.Controllers.Data;
+using projectv2.Shared.Models;
+
+namespace projectv2.Server.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class AccountsController : ControllerBase
+    {
+        private readonly Context _context;
+
+        public AccountsController(Context context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Accounts>> PostAccount(Accounts account)
+        {
+            _context.accounts.Add(account);
+            await _context.SaveChangesAsync();
+
+            return Ok(account);
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Accounts>>> Get()
+        {
+            if (_context.accounts == null)
+            {
+                return NotFound();
+            }
+            return Ok(await _context.accounts.ToListAsync());
+        }
+    }
+}
---
+++ projectv2/Shared/Models/Accounts.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projectv2.Shared.Models
+{
+    public class Accounts
+    {
+        [Required]
+        [StringLength(16, ErrorMessage = "Please enter your first name.", MinimumLength = 3)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(16, ErrorMessage = "Please enter your first name.", MinimumLength = 3)]
+        public string LastName { get; set; }
+        [Key]
+        [Required(ErrorMessage = "Please enter your email address.")]
+        [EmailAddress(ErrorMessage = "Please enter a valid email address.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Please select your gender.")]
+        public string Gender { get; set; }
+
+        [Required(ErrorMessage = "Please enter a password.")]
+        [DataType(DataType.Password)]
+        [StringLength(50, MinimumLength = 8, ErrorMessage = "Password must be between 8 and 50 characters.")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Please confirm your password.")]
+        [DataType(DataType.Password)]
+        [Compare(nameof(Password), ErrorMessage = "Passwords do not match.")]
+        public string ConfirmPassword { get; set; }
+
+        [Range(typeof(bool), "true", "true", ErrorMessage = "You must agree to the terms and conditions.")]
+        public bool AgreeToTerms { get; set; }
+
+        [Required(ErrorMessage = "Please enter your phone number.")]
+        [RegularExpression(@"^\d{10}$", ErrorMessage = "Please enter a valid phone number.")]
+        public double PhoneNumber { get; set; }
+    }
+}
